66ce159dce9f90b219d07ace2983c4b813cdadad
lihong <lihong@jiedaibao.com>
2017-09-17 00:07:23 +0800

    update node version and clean code

--- node_modules/hexo/lib/plugins/filter/before_post_render/backtick_code_block.js
+++ node_modules/hexo/lib/plugins/filter/before_post_render/backtick_code_block.js
@@ -8,15 +8,14 @@ var rBacktick = /(\s*)(`{3,}|~{3,}) *(.*) *\n([\s\S]+?)\s*\2(\n+|$)/g;
 var rAllOptions = /([^\s]+)\s+(.+?)\s+(https?:\/\/\S+|\/\S+)\s*(.+)?/;
 var rLangCaption = /([^\s]+)\s*(.+)?/;
 
-function backtickCodeBlock(data){
-  /* jshint validthis: true */
+function backtickCodeBlock(data) {
   var config = this.config.highlight || {};
   if (!config.enable) return;
 
-  data.content = data.content.replace(rBacktick, function(){
+  data.content = data.content.replace(rBacktick, function() {
     var start = arguments[1];
     var end = arguments[5];
-    var args = arguments[3];
+    var args = arguments[3].split('=').shift();
     var content = arguments[4];
 
     var options = {
@@ -25,22 +24,35 @@ function backtickCodeBlock(data){
       tab: config.tab_replace
     };
 
-    if (args){
+    if (options.gutter) {
+      config.first_line_number = config.first_line_number || 'always1';
+      if (config.first_line_number === 'inline') {
+
+        //setup line number by inline
+        arguments[3] = arguments[3].replace('=+', '=');
+        options.gutter = arguments[3].includes('=');
+
+        // setup fiestLineNumber;
+        options.firstLine = options.gutter ? arguments[3].split('=')[1] || 1 : 0;
+      }
+    }
+
+    if (args) {
       var match;
 
-      if (rAllOptions.test(args)){
+      if (rAllOptions.test(args)) {
         match = args.match(rAllOptions);
-      } else if (rLangCaption.test(args)){
+      } else if (rLangCaption.test(args)) {
         match = args.match(rLangCaption);
       }
 
       if (match) {
         options.lang = match[1];
 
-        if (match[2]){
+        if (match[2]) {
           options.caption = '<span>' + match[2] + '</span>';
 
-          if (match[3]){
+          if (match[3]) {
             options.caption += '<a href="' + match[3] + '">' + (match[4] ? match[4] : 'link') + '</a>';
           }
         }
