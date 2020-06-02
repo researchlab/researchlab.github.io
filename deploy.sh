b8390c8053a02b3368a1d1168f26a7fbbee58bd8
hong li <lihongitchina@gmail.com>
2020-06-02 22:19:00 +0800

    [chore] deploy site

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 
 rm -rf db.json
-rm -rf CNAME
 rm -r $(ls |grep -v deploy.sh |grep -v node_modules | grep -v public)
 mv public/* .
+rm -rf CNAME
 git add .
 git commit -m"Site Update:$(date)"
 git push origin master -vvv
